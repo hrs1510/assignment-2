a97fc6814d836fa80d55628d2c522c3a671bffa3
Himanshu Rani <hrs.1510@gmail.com>
2025-10-19 20:48:08 +0200

    firstFivePostsTitle displayed in console

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 async function fetchPosts() {
-  /*fetch data from jsonPlaceHolder*/
+  // Fetch data from jsonPlaceHolder
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await response.json();
-
   return data;
 }
 
-
-
-/*Using array methods*/
-/*Print the first 5 names of the meals in alphabetical order*/
-function firstFive (posts) {
-  return posts.sort((a, b) => a.title.localeCompare(b.title)).slice(0, 5);
+// Print the first 5 post titles in alphabetical order
+function firstFive(posts) {
+  const firstFivePosts = posts.sort((a, b) => a.title.localeCompare(b.title)).slice(0, 5);
+  firstFivePosts.forEach(post => console.log(post.title));
 }
 
-function main() {
-  const posts = fetchPosts();
-  const firstFive = firstFive(posts);
-  console.log(firstFive);
+async function main() {
+  const posts = await fetchPosts();
+  firstFive(posts);
 }
+
+main();
