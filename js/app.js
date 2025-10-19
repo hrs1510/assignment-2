0220bc502720b8d11f808ce90dc39f286fa9c032
Himanshu Rani <hrs.1510@gmail.com>
2025-10-19 22:19:26 +0200

    post data reshaped according to given task

--- js/app.js
+++ js/app.js
@@ -6,52 +6,65 @@ async function fetchPosts() {
 }
 
 // Print the first 5 post titles in alphabetical order
-function firstFive(posts) {
-  const firstFivePosts = posts.sort((a, b) => a.title.localeCompare(b.title)).slice(0, 5); //first we sorted all posts alphabetically by title, then we took the first 5 posts by .slice() method.
-  firstFivePosts.forEach(post => console.log(post.title)); //print each title of the first five posts
-}
-
-//Print all posts that contain a same userID
-function groupPostsByUserId(posts) {
-  const userId = 3; // here we are filtering posts for userId 3
-  const filteredPosts = posts.filter(post => post.userId === userId).map(post => ({
-    userId: post.userId,
-    body: post.body
-  })); // first we filtered posts by userId, this gives us an array of posts with userId 3, then we mapped over that array to create a new array of objects containing only userId and body properties.
-  console.log(filteredPosts);
-}
+// function firstFive(posts) {
+//   const firstFivePosts = posts.sort((a, b) => a.title.localeCompare(b.title)).slice(0, 5); //first we sorted all posts alphabetically by title, then we took the first 5 posts by .slice() method.
+//   firstFivePosts.forEach(post => console.log(post.title)); //print each title of the first five posts
+// }
+//
+// //Print all posts that contain a same userID
+// function groupPostsByUserId(posts) {
+//   const userId = 3; // here we are filtering posts for userId 3
+//   const filteredPosts = posts.filter(post => post.userId === userId).map(post => ({
+//     userId: post.userId,
+//     body: post.body
+//   })); // first we filtered posts by userId, this gives us an array of posts with userId 3, then we mapped over that array to create a new array of objects containing only userId and body properties.
+//   console.log(filteredPosts);
+// }
 
 //Create a Javascript object that contains how many times a post of each userId appears
-function countPostsByUserId(posts) {
-  const groupedPosts = posts.reduce((obj, post) => { // we are using reduce method to create an object that counts posts by userId
-    if (!obj[post.userId]) { //
-      obj[post.userId] = 0
-    }
-    obj[post.userId] += 1; // increment the count for that userId
-    return obj;
-  }, {});
-  return console.log(groupedPosts);
-}
+// function countPostsByUserId(posts) {
+//   const groupedPosts = posts.reduce((obj, post) => { // we are using reduce method to create an object that counts posts by userId
+//     if (!obj[post.userId]) { //
+//       obj[post.userId] = 0
+//     }
+//     obj[post.userId] += 1; // increment the count for that userId
+//     return obj;
+//   }, {});
+//   return console.log(groupedPosts);
+// }
+
 //Stretch goals (VG)
 //Group by userId
-function groupById(posts,userId) {
-  const grouped = posts.reduce((result, post) => {
-    const groupKey = post[userId]; // get the value of the specified userId property to group by
-    if (!result[groupKey]) { //
-      result[groupKey] = [];
-    }
-    result[groupKey].push(post); // add the post to the appropriate group by userId
-    return result;
-  }, {});
-  console.log(grouped);
+// function groupById(posts, userId) {
+//   const grouped = posts.reduce((result, post) => {
+//     const groupKey = post[userId]; // get the value of the specified userId property to group by
+//     if (!result[groupKey]) { //
+//       result[groupKey] = [];
+//     }
+//     result[groupKey].push(post); // add the post to the appropriate group by userId
+//     return result;
+//   }, {});
+//   console.log(grouped);
+// }
+
+//Select and Reshape data
+function selectAndReshape(posts) {
+  const reShapedPosts = posts.map(post => ({
+    userId: post.userId,
+    postId: post.id,
+    title: post.title.toUpperCase(), // converting title to uppercase
+  }))
+  return console.log(reShapedPosts);
 }
+
 //executing all functions
 async function main() {
   const posts = await fetchPosts();
-  firstFive(posts);
-  groupPostsByUserId(posts);
-  countPostsByUserId(posts);
-  groupById(posts, 'userId');
+  // firstFive(posts);
+  // groupPostsByUserId(posts);
+  // countPostsByUserId(posts);
+  // groupById(posts, 'userId');
+  selectAndReshape(posts);
 
 }
 
