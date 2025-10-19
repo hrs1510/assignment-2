6be06e7a467901d877ad6176a77f14534a946104
Himanshu Rani <hrs.1510@gmail.com>
2025-10-19 21:06:10 +0200

    function to filter posts on userID based compeleted and tested

--- js/app.js
+++ js/app.js
@@ -11,9 +11,20 @@ function firstFive(posts) {
   firstFivePosts.forEach(post => console.log(post.title));
 }
 
+//Print all posts that contain a same userID
+function groupPostsByUserId(posts) {
+  const userId = 3;
+  const filteredPosts = posts.filter(post => post.userId === userId).map(post => ({ userId: post.userId, body: post.body }));
+  console.log(filteredPosts);
+}
+
+
+//executing all functions
 async function main() {
   const posts = await fetchPosts();
   firstFive(posts);
+  groupPostsByUserId(posts);
+
 }
 
 main();
